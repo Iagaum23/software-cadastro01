cbc7fb232a5f5463e1bb363e91f8f571280d5127
Nifuji <iagofbarbosa23@gmail.com>
2020-11-08 15:30:16 -0300

    Add files via upload

--- src/controller/Cadastro.java
+++ src/controller/Cadastro.java
@@ -1,36 +1,46 @@
 package controller;
-	// So nao esta com a acentuacao, pois costuma dar erro quando importado o codigo, ja que nao costuma comportar o teclado UTF-8.
+
+import javax.swing.JOptionPane;
+
+import util.IntegridadeCPF;
+
+// So nao esta com a acentuacao, pois costuma dar erro quando importado o codigo, ja que nao costuma comportar o teclado UTF-8.
 public class Cadastro {
 	// Variaveis globais de uso geral em todas classes.
-	public int opp;
+	public static int opp;
     public int op1;
     // Numero de contas criadas, para assim poder dar uma posição no vetor para a conta.
-    public static int accNum = 0;
+    private static int accNum = 0;
     // Variavel que faz o app ficar rodando.
     public static boolean app;
     public static void main(String[] args) {
        new Cadastro01();
        Login log = new Login();
-       Logado online = new Logado();
        new Contas();
         app = false;
         do {
-           Cadastro01.Cadastro();
+           Cadastro01.Cadastro(accNum);
            if(Cadastro01.contaCriada == true) {
         	   accNum += 1;
            }
            // Try catch so foi usado para o programa continue, mesmo que os valores deem null, que aí o codigo continua rodando.
            try {
-        	   Contas.contas();
+        	   Contas.contas(accNum);
         	 } catch (NullPointerException nexc) {
         	   Contas.contasVazias();
         	 }  finally {
         		 Contas.contasVazias();
         	 }
-           log.LoginUser();
-           online.logado();
+           log.LoginUser(accNum);
+           logado(accNum);
         } while (!app);
     }
-   
+	private static void logado(int matricula) {
+		Object[] op2 = {"Voltar ao menu inicial", "Sair"}; 
+		opp = JOptionPane.showOptionDialog(null, "Olá Sr(a)"+Login.accLogged[0]+"\nE-mail: "+Login.accLogged[1]+" \nDono do CPF:\t"+IntegridadeCPF.imprimeCPF(Login.accLogged[2])+"\n\tMatricula:"+matricula+"\nO que o Sr(a) gostaria de fazer ?", null, JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, op2, op2[0]);
+         if ((String)op2[opp] == "Sair") {
+             System.exit(0);
+         }
+	}
     
 }
--- src/controller/Contas.java
+++ src/controller/Contas.java
@@ -3,16 +3,16 @@ package controller;
 import util.IntegridadeCPF;
 
 public class Contas {
-	public static void contas() {
+	public static void contas(int totalContas) {
 		new Login();
 		// Essa classe so foi criada para ter uma parte separada para si, para aplicar uma manutencao nela quando necessaria.
-		System.out.println("\t---Contas atuáis---\t");
+		System.out.println("\t---Contas atuáis---\t\n\tContas totais:"+totalContas);
 		for (int x = 0; x < 5; x++) {
-			System.out.println("Nomes de usuários:\t\""+Login.nome[x]+"\"\tSenhas:\t\""+Login.senha[x]+"\"\tEmail:\t\""+Login.email[x]+"\"\nCPF:"+IntegridadeCPF.imprimeCPF(Login.cpf[x]));
+			System.out.println(x+" matricula|\nNomes de usuarios:\t\""+Login.nome[x]+"\"\tSenhas:\t\""+Login.senha[x]+"\"\tEmail:\t\""+Login.email[x]+"\"\nCPF:"+IntegridadeCPF.imprimeCPF(Login.cpf[x])+"\n");
 		}
 		System.out.println();
 	}
 	public static void contasVazias() {
-		System.out.println("Nomes de usuários:\t\"Null\"\tSenhas:\t\"Null\"\tEmail:\t\"Null\"\nCPF:Null");
+		System.out.println("Sem conta cadastrada|\nNomes de usuários:\t\"Null\"\tSenhas:\t\"Null\"\tEmail:\t\"Null\"\nCPF:Null\n");
 	}
 }
